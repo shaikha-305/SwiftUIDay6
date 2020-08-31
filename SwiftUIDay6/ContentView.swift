652ff12569195b2f40afc170f03e3c5ef3a625b2
shaikha-305 <shaikhaaljenaidel@gmail.com>
2020-08-31 11:16:36 +0300

    Initial Commit

---
+++ SwiftUIDay6/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDay6
+//
+//  Created by shaikha aljenaidel on 8/31/20.
+//  Copyright © 2020 shaikha aljenaidel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
